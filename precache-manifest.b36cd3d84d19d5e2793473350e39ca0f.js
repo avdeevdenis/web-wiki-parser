ece75729a9acb3276ea59c9ae9d402de4ab9b443
Avdeev Denis <av-denis@yandex-team.ru>
2019-09-29 21:43:35 +0300

    Updates

---
+++ precache-manifest.b36cd3d84d19d5e2793473350e39ca0f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dfcd4d217235ff223cd1c213640be10",
+    "url": "/web-wiki-parser/static/media/1Pt_g8LJRfWJmhDAuUsSQamb1W0lwk4S4WjMDrcfJh1Zyc61YBlG.1dfcd4d2.woff"
+  },
+  {
+    "revision": "74626c1995e211eb67b8",
+    "url": "/web-wiki-parser/static/css/main.4b34fa7e.chunk.css"
+  },
+  {
+    "revision": "f139d1decbe882211bd8",
+    "url": "/web-wiki-parser/static/js/1.f139d1de.chunk.js"
+  },
+  {
+    "revision": "e59ae71164b1d36b46fc",
+    "url": "/web-wiki-parser/static/js/runtime~main.e59ae711.js"
+  },
+  {
+    "revision": "4b5bdab4756c163d4c18ac5e12ebd4e6",
+    "url": "/web-wiki-parser/static/media/1Pt_g8LJRfWJmhDAuUsSQamb1W0lwk4S4WjMDr4fJh1Zyc61YBlG.4b5bdab4.woff"
+  },
+  {
+    "revision": "74626c1995e211eb67b8",
+    "url": "/web-wiki-parser/static/js/main.74626c19.chunk.js"
+  },
+  {
+    "revision": "54f4d2823a42074c68181d5772c4e49b",
+    "url": "/web-wiki-parser/static/media/1Pt_g8LJRfWJmhDAuUsSQamb1W0lwk4S4WjMDr0fJh1Zyc61YBlG.54f4d282.woff"
+  },
+  {
+    "revision": "12e906709c596ec6fd9444caa31dc57e",
+    "url": "/web-wiki-parser/static/media/1Pt_g8LJRfWJmhDAuUsSQamb1W0lwk4S4WjMDrwfJh1Zyc61YBlG.12e90670.woff"
+  },
+  {
+    "revision": "c6b31663e5f5c97f37a7194ae10eba8e",
+    "url": "/web-wiki-parser/static/media/1Pt_g8LJRfWJmhDAuUsSQamb1W0lwk4S4WjMDrAfJh1Zyc61YBlG.c6b31663.woff"
+  },
+  {
+    "revision": "577d3b7723e8bc200cdd5747df3847a4",
+    "url": "/web-wiki-parser/static/media/1Pt_g8LJRfWJmhDAuUsSQamb1W0lwk4S4WjMDrMfJh1Zyc61YA.577d3b77.woff"
+  },
+  {
+    "revision": "4fd089a1773b03fbc0fd7a59c917964f",
+    "url": "/web-wiki-parser/index.html"
+  }
+];
